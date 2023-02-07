a555f087887c630361e95bc27502b7a0067d2401
Ege Kayra <egekayra.business@gmail.com>
2023-02-07 21:49:33 +0300

    Initial commit

---
+++ src/main/java/com/kyraymege/jwtauth/service/AuthenticationService.java
@@ -0,0 +1,51 @@
+package com.kyraymege.jwtauth.service;
+
+import com.kyraymege.jwtauth.dto.AuthenticationRequest;
+import com.kyraymege.jwtauth.dto.AuthenticationResponse;
+import com.kyraymege.jwtauth.dto.RegisterRequest;
+import com.kyraymege.jwtauth.model.Role;
+import com.kyraymege.jwtauth.model.User;
+import com.kyraymege.jwtauth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
